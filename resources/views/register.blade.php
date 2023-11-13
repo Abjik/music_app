a9a5b32ab9ffa6ab9f3a9a34e84f7d887222ad0c
Abzhik <52734118+Abjik@users.noreply.github.com>
2023-11-13 13:54:31 +0600

    initial

---
+++ resources/views/register.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3 class="panel-title">Login</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <form method="POST" action="">
+                                    {{ csrf_field() }}
+                                    <div class="form-group">
+                                        <label for="email">Email</label>
+                                        <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}">
+                                    </div>
+                                    <div class="form-group">
+    </div>
+</body>
+</html>
